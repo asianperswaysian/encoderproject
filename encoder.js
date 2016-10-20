2b67484270892d93490dc42cf6d5b29fe248bf00
Niko Vergara <asianperswaysian.13@gmail.com>
2016-10-20 09:03:48 -0700

    Im doing wha you showed

--- encoder.js
+++ encoder.js
@@ -1,5 +1,5 @@
 
-var letters {
+var letters = {
   65:"a",
   66:"b",
   67:"c",
@@ -28,5 +28,40 @@ var letters {
   90:"z"
 }
 $("#inputArea").keydown(function(e){
+
+if ($("input:checked").val() == "echo") {
+  $("#textArea").append(translation[e.keycode]);
+}
+
+if($("input:checked").val() == "shiftcipher") {
+  // very like echo, shift uno  back
+}
+
+if($("input:checked").val() == "heiroglyphics") {
+  $("#textArea").append("<img src= 'images/heiroglyphics" + something + ".gif>");
+}
+
+if($("input:checked").val() == "runes") {
+
+}
+});
+
+$("#translationInputText").keydown(function(e){
+  if(e.keycode == 13) {
+// 1. grab value of input text box
+
+    // 2. turn strin in text box into array
+    //inputTextString.split("")
+    // z m m
+
+    // 3. loop through the array and translate each letter
+
+    // 4. handy way to go from letter to number: letterVariable.charCodeAt(0)
+  }
+})
+
+$("input[type='radio']".click(function){
+  $("input:checked").prop('checked',false)
+  $(this).prop('checked',true)
   $("#textArea").append.(letters[e.keyCode]);
 });
